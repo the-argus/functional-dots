66319bb4c42c02d178b403fd0abc2e1d6261a031
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-02-27 19:11:12 -0500

    added arkenfox overrides

---
+++ Firefox Private/user-overrides.js
@@ -0,0 +1,10 @@
+// homepage
+user_pref("browser.startup.homepage", "about:home");
+user_pref("browser.newtabpage.enabled", true);
+user_pref("browser.startup.page", 1);
+
+// disable the "master switch" that disables about:home
+//user_pref("browser.startup.homepage_override.mstone", "");
+
+// allow search engine searching from the urlbar
+user_pref("keyword.enabled", true);
