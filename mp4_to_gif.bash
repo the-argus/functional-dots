1242a0d5ffacd9953bcd0812f8ddb39f72a3d2c4
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-03-28 22:27:15 +0000

    added by own script

--- mp4_to_gif.bash
+++ mp4_to_gif.bash
@@ -1,2 +1,21 @@
-ffmpeg -y -i input.mp4 -vf fps=10,scale=716:-1:flags=lanczos,palettegen palette.png
-ffmpeg -i input.mp4 -i palette.png -filter_complex "fps=10,scale=716:-1:flags=lanczos[x];[x][1:v]paletteuse" output.gif
+# original
+# ffmpeg -y -i input.mp4 -vf fps=10,scale=716:-1:flags=lanczos,palettegen palette.png
+# ffmpeg -i input.mp4 -i palette.png -filter_complex "fps=10,scale=716:-1:flags=lanczos[x];[x][1:v]paletteuse" output.gif
+
+PALETTE=/tmp/palette.png
+
+X_RES="$3"
+Y_RES="$4"
+
+# get palette
+ffmpeg -y \
+    -i "$1" \
+    -vf fps=10,scale=$X_RES:$Y_RES:flags=lanczos,palettegen \
+    "$PALETTE";
+
+# final conversion
+ffmpeg \
+    -i "$1" \
+    -i "$PALETTE" \
+    -filter_complex "fps=10,scale="$X_RES":"$Y_RES":flags=lanczos[x];[x][1:v]paletteuse" \
+    "$2";
